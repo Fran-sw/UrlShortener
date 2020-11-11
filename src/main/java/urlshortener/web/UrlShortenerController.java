a42e7d64d879caeb4cf20b9395918460e815dd56
Fran-sw <franmabad@gmail.com>
2020-11-11 17:39:45 +0100

    Check User agents on shorturl and qr

--- src/main/java/urlshortener/web/UrlShortenerController.java
+++ src/main/java/urlshortener/web/UrlShortenerController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -87,9 +88,12 @@ public class UrlShortenerController {
   public ResponseEntity<ShortURL> shortener(@RequestParam("url") String url,
                                             @RequestParam(value = "sponsor", required = false)
                                             String sponsor,
+                                            @RequestHeader(value = "User-Agent") String userAgent,
                                             HttpServletRequest request) {
     UrlValidator urlValidator = new UrlValidator(new String[] {"http",
         "https"});
+    //We get user agants
+    shortUrlService.processAgents(userAgent);
     if (urlValidator.isValid(url)) {
       ShortURL su = shortUrlService.save(url, sponsor, request.getRemoteAddr());
       HttpHeaders h = new HttpHeaders();
