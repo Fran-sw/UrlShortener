4d633654c04cb80822332bca5159eda2c1a26736
fran-sw <franmabad@gmail.com>
2020-12-23 18:42:48 +0100

    Test working finally

--- src/test/java/urlshortener/web/UserAgentsTest.java
+++ src/test/java/urlshortener/web/UserAgentsTest.java
@@ -23,6 +23,9 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.StreamingHttpOutputMessage.Body;
 import org.mockito.stubbing.Answer;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -31,12 +34,17 @@ import urlshortener.service.ClickService;
 import urlshortener.service.ShortURLService;
 import urlshortener.service.ServiceAgents;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 import java.util.*;
+
+import javax.swing.text.AbstractDocument.Content;
 
 
 public class UserAgentsTest {
 
   private MockMvc mockMvc;
+  private MockMvc mockMvc2;
 
   @Mock
   private ClickService clickService;
@@ -48,43 +56,52 @@ public class UserAgentsTest {
   private ServiceAgents serviceAgents;
 
   @InjectMocks
-  private UrlShortenerAgentsController userAgents;
+  private UrlShortenerController urlShortener;
+
+  @InjectMocks
+  private UrlShortenerAgentsController agentsCont;
 
   @Before
   public void setup() {
     MockitoAnnotations.initMocks(this);
-    this.mockMvc = MockMvcBuilders.standaloneSetup(userAgents).build();
+    this.mockMvc = MockMvcBuilders.standaloneSetup(urlShortener).build();
+    this.mockMvc2 = MockMvcBuilders.standaloneSetup(agentsCont).build();
+  }
+
+  // CONFIRM THAT WITH USER AGENTS != null -> is invoked process of it
+  @Test
+  public void onClickInvokesAgents() throws Exception{
+    
+    mockMvc.perform(get("/{id}", "f684a3c4").header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"))
+        .andDo(print());
+
+    verify(serviceAgents).processAgents(any());
   }
 
   @Test
-  @Ignore
-  public void userAgentsTest()
+  public void checkThatCalculationAreOk()
       throws Exception {
-        when(serviceAgents.getAgentsInfo()).thenReturn(createMap());
 
-        // SHOULD THERE BE AN OTHER OK FOR URLSERIE? -> maybe they share a lot with shortener test????
-        // Index.html shows that might be the case
+        //We try to get the info -> After it calculateTop5 should have been invoked
+        // Also expect the list completly clear since it hasnt had time to calculate top 5
+        mockMvc2.perform(get("/agentsInfo").header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"))
+        .andDo(print())
+        .andExpect(content().json("{}"));
 
-        // We post an url -> there should not be a count on user agents
-        mockMvc.perform(
-          post("/link").param("url", "http://example.com/").param(
-          "sponsor", "http://sponsor.com/"))
-          .andDo(print());
+        verify(serviceAgents).calculateTop5();
 
-        // We perfom a get to the just created url -> it will start a count of user agents
-        mockMvc.perform(
-          get("/f684a3c4"))
-          .andDo(print());
-
-        // There will be 5 sec delay between each calculation of the top5 useragents
+        //wait 5 sec... and repeat but diferent outcome!
         Thread.sleep(5000);
 
-        // We need to get the user agents
-        mockMvc.perform(
-          get("/agentsInfo"))
-          .andDo(print());
+        mockMvc2.perform(get("/agentsInfo").header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"))
+        .andDo(print())
+        .andExpect(content().json("{}"));
+
+        verify(serviceAgents,times(2)).calculateTop5();
       
   }
+
+  
   private Map<String,Integer> createMap(){
     Map<String, Integer> m = new HashMap<String, Integer>();
     m.put("Win10",1);
