0899efe53b9206a1abce34d11840ede173ea9375
fran-sw <franmabad@gmail.com>
2020-12-24 11:02:34 +0100

    Started qr tests

--- src/test/java/urlshortener/fixtures/ShortURLFixture.java
+++ src/test/java/urlshortener/fixtures/ShortURLFixture.java
@@ -67,4 +67,15 @@ public class ShortURLFixture {
       return null;
     }
   }
+
+  public static ShortURL shortURL3() {
+    try{
+      URI uri = new URI("http://localhost/f684a3c4");
+      return new ShortURL("f684a3c4", "http://example.com/", uri, null, null,
+      null, null, true, "127.0.0.1", null,null,"http://localhost:8080/qr/f684a3c4");
+    }
+   catch (Exception e) {
+      return null;
+    }
+  }
 }
